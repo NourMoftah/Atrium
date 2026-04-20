935340cb7926743e75a5db4b870bb2a9ba4857d2
Bassant Hossny <bassanthossny177@gmail.com>
2026-04-20 16:17:54 +0200

    Add dashboard component

--- frontend-angular/src/app/components/register/register.ts
+++ frontend-angular/src/app/components/register/register.ts
@@ -86,7 +86,6 @@ export class Register {
     const input = event.target as HTMLInputElement;
     if (input.files && input.files.length > 0) {
       const file = input.files[0];
-      // تحديث قيمة الفورم بالملف المختار
       this.registerForm.patchValue({ idFile: file });
       console.log('Selected file:', file.name);
     }
