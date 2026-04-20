2f7f7a0b3b7762ee70b30a200a13b910d1e4b643
Loay Abdelkader <loay.ali.emam.ghonem@gmail.com>
2026-04-20 19:57:27 +0000

    Add Get All Functionality Endpoint

--- atrium/src/order/order.controller.ts
+++ atrium/src/order/order.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Request, Response } from '@nestjs/common';
+
+import { OrderService } from './order.service';
 
 @Controller('order')
-export class OrderController {}
+export class OrderController {
+    constructor(private orderService: OrderService) { }
+
+    @Get('orders')
+    getOrders(@Request() req: any, @Response() res: any) {
+        return this.orderService.getAll(req, res);
+    }
+}
